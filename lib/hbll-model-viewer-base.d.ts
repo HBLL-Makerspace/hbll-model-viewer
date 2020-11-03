ab9a3967ad6b84fa7a07af867d99f3a089285284
Benjamin Geurt Brenkman <bbrenk@byu.edu>
2020-11-03 14:39:32 -0700

    started work on implementing functionality

--- lib/hbll-model-viewer-base.d.ts
+++ lib/hbll-model-viewer-base.d.ts
@@ -2,5 +2,7 @@ import { LitElement } from "lit-element";
 export default class HbllModelViewerElementBase extends LitElement {
     constructor();
     src: string | null;
+    skybox_image: string | null;
+    static get styles(): import("lit-element").CSSResult;
     render(): import("lit-element").TemplateResult;
 }
--- src/hbll-model-viewer-base.ts
+++ src/hbll-model-viewer-base.ts
@@ -1,18 +1,37 @@
-import { property, LitElement, html } from "lit-element";
+import { html, LitElement, property, css } from "lit-element";
+
+const map_style = {
+  height: "100vh",
+  width: "100%",
+};
 
 export default class HbllModelViewerElementBase extends LitElement {
   constructor() {
     super();
-    console.log("Created HbllModelViewerElement");
+    console.log("Created HbllModelViewerElement v10");
   }
 
   @property({ type: String }) src: string | null = null;
+  @property({ type: String }) skybox_image: string | null = null;
+
+  static get styles() {
+    return css`
+      model-viewer {
+        width: 100%;
+        height: 100vh;
+      }
+    `;
+  }
 
   render() {
     return html`
-      <div>
-        <p>A paragraph</p>
-      </div>
+      <model-viewer
+        style=${map_style}
+        src=${this.src}
+        skybox-image=${this.skybox_image}
+        camera-controls
+      >
+      </model-viewer>
     `;
   }
 }
